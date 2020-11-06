c5f5580105f9d08e5b4830195833bbbfc49c017c
claudio.caniullan <claudio.caniullan@usach.cl>
2020-11-06 15:53:08 -0300

    agregan funciones base

--- proyecto_ahorcado.py
+++ proyecto_ahorcado.py
@@ -78,3 +78,16 @@ WORDS = [
     'computadora',
     'teclado'
 ]
+
+
+# selecciona palabra de la lista WORDS
+def random_word():
+    idx = random.randint(0, len(WORDS) - 1)
+    return WORDS[idx]
+
+# imprime pantallas
+def display_board(hidden_word, tries):
+    print(IMAGES[tries])
+    print('')
+    print(hidden_word)
+    print('--- * --- * --- * --- * --- * --- ')
