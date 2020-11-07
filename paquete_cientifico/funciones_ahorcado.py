e5b6ad06d9ed04b6eb8da3912209646ec8bea899
Maguilera85 <drianketing@gmail.com>
2020-11-07 18:47:50 -0300

    se completó el paquete cientifico

---
+++ paquete_cientifico/funciones_ahorcado.py
@@ -0,0 +1,91 @@
+import random
+
+IMAGES = ['''
+
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   /    |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   / \  |
+        =========''', '''
+''']
+WORDS = [
+    'lavadora',
+    'secadora',
+    'sofa',
+    'gobierno',
+    'diputado',
+    'democracia',
+    'computadora',
+    'teclado'
+]
+
+
+# selecciona palabra de la lista WORDS
+def random_word():
+    idx = random.randint(0, len(WORDS) - 1)
+    return WORDS[idx]
+
+# imprime pantallas
+def display_board(hidden_word, tries):
+    print(IMAGES[tries])
+    print('')
+    print(hidden_word)
+    print('--- * --- * --- * --- * --- * --- ')
--- proyecto_ahorcado.py
+++ proyecto_ahorcado.py
@@ -1,107 +1,15 @@
 import random
-
-
-IMAGES = ['''
-
-    +---+
-    |   |
-        |
-        |
-        |
-        |
-        =========''', '''
-
-    +---+
-    |   |
-    O   |
-        |
-        |
-        |
-        =========''', '''
-
-    +---+
-    |   |
-    O   |
-    |   |
-        |
-        |
-        =========''', '''
-
-    +---+
-    |   |
-    O   |
-   /|   |
-        |
-        |
-        =========''', '''
-
-    +---+
-    |   |
-    O   |
-   /|\  |
-        |
-        |
-        =========''', '''
-
-    +---+
-    |   |
-    O   |
-   /|\  |
-    |   |
-        |
-        =========''', '''
-
-    +---+
-    |   |
-    O   |
-   /|\  |
-    |   |
-   /    |
-        =========''', '''
-
-    +---+
-    |   |
-    O   |
-   /|\  |
-    |   |
-   / \  |
-        =========''', '''
-''']
-
-WORDS = [
-    'lavadora',
-    'secadora',
-    'sofa',
-    'gobierno',
-    'diputado',
-    'democracia',
-    'computadora',
-    'teclado'
-]
-
-
-# selecciona palabra de la lista WORDS
-def random_word():
-    idx = random.randint(0, len(WORDS) - 1)
-    return WORDS[idx]
-
-# imprime pantallas
-def display_board(hidden_word, tries):
-    print(IMAGES[tries])
-    print('')
-    print(hidden_word)e
-    print('--- * --- * --- * --- * --- * --- ')
-
+import paquete_cientifico as pc
 
 
 def run():
-    word = random_word()
+    word = pc.random_word()
     hidden_word = ['-'] * len(word)
     tries = 0
 
     while True:
         # muestra el tablero
-        display_board(hidden_word, tries)
+        pc.display_board(hidden_word, tries)
         current_letter = str(input('Escoge una letra: '))
          
         # se crea lista para indexar letras
@@ -118,9 +26,9 @@ def run():
 
             # al septimo intento se pierde la jugada
             if tries == 7:
-                display_board(hidden_word, tries)
+                pc.display_board(hidden_word, tries)
                 print('')
-                print('Â¡Perdiste! La palabra correcta era {}'.format(word))
+                print('Â¡Perdiste! La palabra correcta era ' + word)
                 break
         else:
             for idx in letter_indexes: # la letra del usuario es replazada en hidenword
@@ -129,14 +37,13 @@ def run():
             # vaciamos esta lista para volver a comenzar el while, ESTUDIAR
             letter_indexes = []
 
-        # encontrar al ganador, terminar el juego 
+        # encontrar al ganador, terminar el juego // INTENTAR MODIFICAR ESTO POR UN IF/ELSE
         try:
-            hidden_word.index('-')
+            hidden_word.index('-') #este metodo recibe un elemento como argumento y devuelve un indeci de su primera aparicion en su lista
         except ValueError:
             print('')
-            print('Â¡Felicidades! Ganaste. La palabra es: {}'.format(word))
+            print('¡Felicidades! Ganaste. La palabra es: ' + word)
             break
-
 
 if __name__ == '__main__':
     print('B I E N V E N I D O S  A  A H O R C A D O S')
